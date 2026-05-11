a2ef51be39c8fcfc0c6a77803cc12078e7c4dc07
庞梦鑫 <2823801378@qq.com>
2026-05-11 17:25:17 +0800

    修复llamafactory调用线程过多卡死的问题

--- eval/config.py
+++ eval/config.py
@@ -6,7 +6,7 @@ DEEPSEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
 
 # 模型配置
 ORIGINAL_MODEL_PATH = "../model/models--Qwen--Qwen3.5-0.8B-Base/snapshots/a9a407bcae463285164cc9133995c515379cebe5"
-FINE_TUNED_MODEL_PATH = "../LlamaFactory/saves/Qwen3.5-0.8B-Base/lora/train_2026-04-12-00-04-45/checkpoint-42"
+FINE_TUNED_MODEL_PATH = "../saves/Qwen3.5-0.8B-Base/lora/train_2026-05-06-20-44-32/checkpoint-42"
 
 # 评估配置
 QUESTION_COUNT = 10  # 生成的问题数量
